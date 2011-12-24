299a3cf517739298f0d52cb5c34e97830c6a9f0a
Валентин Немцев <mail@valentin-nemcev.com>
2011-12-25 03:06:37 +0400

    Fix errors; cleanup and rearrange source; invert light version

--- mintty-solarized-dark.sh
+++ mintty-solarized-dark.sh
@@ -1,23 +1,20 @@
+echo -ne '\e]4;08;#002B36\a'  # bold black   -> base03
+echo -ne '\e]4;00;#073642\a'  # black        -> base02
+echo -ne '\e]4;10;#586E75\a'  # bold green   -> base01
+echo -ne '\e]4;11;#657B83\a'  # bold yellow  -> base00
+echo -ne '\e]4;12;#839496\a'  # bold blue    -> base0
+echo -ne '\e]4;14;#93A1A1\a'  # bold cyan    -> base1
+echo -ne '\e]4;07;#EEE8D5\a'  # white        -> base2
+echo -ne '\e]4;15;#FDF6E3\a'  # bold white   -> base3
+echo -ne '\e]4;03;#B58900\a'  # yellow       -> yellow
+echo -ne '\e]4;09;#CB4B16\a'  # bold red     -> orange
+echo -ne '\e]4;01;#DC322F\a'  # red          -> red
+echo -ne '\e]4;05;#D33682\a'  # magenta      -> magenta
+echo -ne '\e]4;13;#6C71C4\a'  # bold magenta -> violet
+echo -ne '\e]4;04;#268BD2\a'  # blue         -> blue
+echo -ne '\e]4;06;#2AA198\a'  # cyan         -> cyan
+echo -ne '\e]4;02;#859900\a'  # green        -> green
 
-# solarized dark
-echo -ne '\e]4;0;#073642\a'   # black
-echo -ne '\e]4;1;#dc322f\a'   # red
-echo -ne '\e]4;2;#859900\a'   # green
-echo -ne '\e]4;3;#b58900\a'   # yellow
-echo -ne '\e]4;4;#268bd2\a'   # blue
-echo -ne '\e]4;5;#d33682\a'   # magenta
-echo -ne '\e]4;6;#2aa198\a'   # cyan
-echo -ne '\e]4;7;#eee8d5\a'   # white (light grey really)
-echo -ne '\e]4;8;#002b36\a'   # bold black (i.e. dark grey)
-echo -ne '\e]4;9;#cb4b16\a'   # bold red
-echo -ne '\e]4;10;#586e75\a'  # bold green
-echo -ne '\e]4;11;#657b83\a'  # bold yellow
-echo -ne '\e]4;12;#839496\a'  # bold blue
-echo -ne '\e]4;13;#6c71c4\a'  # bold magenta
-echo -ne '\e]4;14;#93a1a1\a'  # bold cyan
-echo -ne '\e]4;15;#fdf6e3\a'  # bold white
-
-echo -ne '\e]10;#eee8d5\a'  # foreground
-echo -ne '\e]11;#002b36\a'  # background
-echo -ne '\e]12;#859900\a'  # cursor
-
+echo -ne '\e]12;#93A1A1\a'    # Cursor       -> base1
+echo -ne '\e]10;#839496\a'    # Foreground   -> base0
+echo -ne '\e]11;#002B36\a'    # Background   -> base03
--- mintty-solarized-light.sh
+++ mintty-solarized-light.sh
@@ -1,23 +1,20 @@
+echo -ne '\e]4;08;#FDF6E3\a'  # bold black   -> base3
+echo -ne '\e]4;00;#EEE8D5\a'  # black        -> base2
+echo -ne '\e]4;10;#93A1A1\a'  # bold green   -> base1
+echo -ne '\e]4;11;#839496\a'  # bold yellow  -> base0
+echo -ne '\e]4;12;#657B83\a'  # bold blue    -> base00
+echo -ne '\e]4;14;#586E75\a'  # bold cyan    -> base01
+echo -ne '\e]4;07;#073642\a'  # white        -> base02
+echo -ne '\e]4;15;#002B36\a'  # bold white   -> base03
+echo -ne '\e]4;03;#B58900\a'  # yellow       -> yellow
+echo -ne '\e]4;09;#CB4B16\a'  # bold red     -> orange
+echo -ne '\e]4;01;#DC322F\a'  # red          -> red
+echo -ne '\e]4;05;#D33682\a'  # magenta      -> magenta
+echo -ne '\e]4;13;#6C71C4\a'  # bold magenta -> violet
+echo -ne '\e]4;04;#268BD2\a'  # blue         -> blue
+echo -ne '\e]4;06;#2AA198\a'  # cyan         -> cyan
+echo -ne '\e]4;02;#859900\a'  # green        -> green
 
-# solarized light
-echo -ne '\e]4;0;#073642\a'   # black
-echo -ne '\e]4;1;#dc322f\a'   # red
-echo -ne '\e]4;2;#859900\a'   # green
-echo -ne '\e]4;3;#b58900\a'   # yellow
-echo -ne '\e]4;4;#268bd2\a'   # blue
-echo -ne '\e]4;5;#d33682\a'   # magenta
-echo -ne '\e]4;6;#2aa198\a'   # cyan
-echo -ne '\e]4;7;#eee8d5\a'   # white (light grey really)
-echo -ne '\e]4;8;#002b36\a'   # bold black (i.e. dark grey)
-echo -ne '\e]4;9;#cb4b16\a'   # bold red
-echo -ne '\e]4;10;#586e75\a'  # bold green
-echo -ne '\e]4;11;#657b83\a'  # bold yellow
-echo -ne '\e]4;12;#839496\a'  # bold blue
-echo -ne '\e]4;13;#6c71c4\a'  # bold magenta
-echo -ne '\e]4;14;#93a1a1\a'  # bold cyan
-echo -ne '\e]4;15;#fdf6e3\a'  # bold white
-
-echo -ne '\e]10;#586e75\a'  # foreground
-echo -ne '\e]11;#fdf6e3\a'  # background
-echo -ne '\e]12;#eee8d5\a'  # cursor
-
+echo -ne '\e]12;#586E75\a'    # Cursor       -> base01
+echo -ne '\e]10;#657B83\a'    # Foreground   -> base00
+echo -ne '\e]11;#FDF6E3\a'    # Background   -> base3
